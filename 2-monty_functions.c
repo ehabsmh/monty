0213fc632e0b367081749a733af8ad6d7bf680cb
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-22 02:51:29 +0300

    pchar opcode added

--- 2-monty_functions.c
+++ 2-monty_functions.c
@@ -57,11 +57,15 @@ void pchar(stack_t **stack, unsigned int line_number)
 	if (!*stack)
 	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
 	}
 
 	if ((*stack)->n >= 0 && (*stack)->n <= 127)
 		printf("%c\n", (*stack)->n);
 	else
+	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 }
