930d8262b1a1ec379666cc3948d64912beb753a7
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-22 01:54:43 +0300

    testing something

--- 1-monty_functions.c
+++ 1-monty_functions.c
@@ -9,15 +9,20 @@
 */
 void swap(stack_t **stack, unsigned int line_number)
 {
-	int temp = 0;
+	stack_t *top = *stack, *next;
+	int temp;
 
-	if (!(*stack)->next)
+	if (top)
 	{
-		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		next = top->next;
+		if (next)
+		{
+			temp = top->n;
+			top->n = next->n;
+			next->n = temp;
+			return;
+		}
 	}
-
-	temp = (*stack)->n;
-	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = temp;
+	fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+	exit(EXIT_FAILURE);
 }
