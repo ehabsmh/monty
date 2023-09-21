1a75824c90f5aa17b7a7ae2a2b212e166cd63558
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-22 02:04:30 +0300

    Refactored

--- 1-monty_functions.c
+++ 1-monty_functions.c
@@ -12,18 +12,20 @@ void swap(stack_t **stack, unsigned int line_number)
 	stack_t *top = *stack, *nth_2 = NULL;
 	int temp = 0;
 
-	if (!top->next)
+	if (top)
+	{
+		nth_2 = top->next;
+		
+		if (nth_2)
+		{
+			temp = top->n;
+			top->n = nth_2->n;
+			nth_2->n = temp;
+		}
+	}
+	else
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
-	}
-
-	nth_2 = top->next;
-
-	if (nth_2)
-	{
-		temp = top->n;
-		top->n = nth_2->n;
-		nth_2->n = temp;
 	}
 }
