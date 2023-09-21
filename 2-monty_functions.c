f9e8ada979e79781402838ff31ed5c89470b5a94
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-22 02:38:57 +0300

    .

--- 2-monty_functions.c
+++ 2-monty_functions.c
@@ -4,15 +4,17 @@ void _div(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top = *stack, *nth_2 = NULL;
 
-	if (top->n == 0)
-	{
-		fprintf(stderr, "L%u: division by zero\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+	
 	if (top)
 	{
 		nth_2 = top->next;
 
+		if (top->n == 0)
+		{
+			fprintf(stderr, "L%u: division by zero\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+		
 		if (nth_2)
 		{
 			nth_2->n /= top->n;
