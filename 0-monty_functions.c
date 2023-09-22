31b5597747be8fffcd740b9accbee9d55fea912d
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-22 03:25:23 +0300

    .

--- 0-monty_functions.c
+++ 0-monty_functions.c
@@ -90,7 +90,7 @@ void pall(stack_t **stack, unsigned int line_number)
 */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	if (!*stack)
+	if (!*stack || !stack)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
